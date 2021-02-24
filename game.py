92d89023fad719d8e9b12a861b37a7e06c6f21c4
Juan <juanciflow25@gmail.com>
2021-02-24 17:02:39 -0400

    new

--- game.py
+++ game.py
@@ -2,13 +2,17 @@ import random
 def guess(x):
     random_number = random.randint(1, x)
     guess = 0
+    guesses = 0
     while guess != random_number:
         guess = int(input(f'Guess a number between 1, {x} '))
         if guess > random_number:
-            print("Too high, Try again.")
+            print("Too high, Try again!")
+            guesses = guesses + 1
         elif guess < random_number:
             print("Too low, Try again.")
-    
-    print("You got it!!")
-guess(x = random.randint(1, 1000))
+            guesses = guesses + 1
 
+    print("You got it!! It only took you " + str(guesses) + " guesses!")
+    
+
+guess(x = random.randint(1,1000))
